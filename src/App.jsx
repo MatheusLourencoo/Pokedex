9189ac49a96597bc7a0ebc2d385fedd57d7a4436
Matheus De Oliveira Lourenço <matheuslourencoo@outlook.com>
2026-04-03 10:42:26 -0300

    Adicionado filtro de busca de pokemons
    Criado a lógica no App.jsx com useState e filter para pesquisar por nome, id ou tipo

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,55 @@ import imagemPokedexLogo from './assets/imagemPokedexLogo.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+  // filtro da busca dos pokemons
+  const [filtro, setFiltro] = useState("");
+
+  const listaFiltrada = data.filter((pokemon) => {
+
+    const textoDigitado = filtro.toLowerCase().trim();
+
+    //serve para transformar o texto em minusculo e tirar espaços em brancos
+    if(textoDigitado === ""){
+      return true;
+    }
+    
+    const filtroNome = pokemon.name.toLowerCase().includes(textoDigitado);
+    
+    const filtroId = pokemon.id.toString().includes(textoDigitado);
+
+    const filtroElemento = pokemon.tipo.some((elemento) => {
+      return elemento.toLowerCase().includes(textoDigitado);
+    });
+
+    //vai retornar uma das 3 categorias na busca feito pelo usuario
+    return filtroNome || filtroId || filtroElemento;
+  }); 
 
   return (
     <>
-
       <img className='logoPokedex' src={imagemPokedexLogo} alt='logo Pokedex'/>
 
-    <input placeholder='Adicione a sua pesquisa' className='pesquisa'></input>
+      {}
+      <input 
+        placeholder='Adicione a sua pesquisa' 
+        className='pesquisa'
+        value={filtro}
+        onChange={(evento) => setFiltro(evento.target.value)}
+      />
 
-    <div className='container' >
-      {data.map((pokemon) => (
-        <Card key={pokemon.id}
-        imagem={pokemon.imagem} 
-        nome={pokemon.name}
-        id={pokemon.id}
-        tipo={pokemon.tipo}
-        />
-      ))}
-
+      <div className='container' >
+        {}
+        {listaFiltrada.map((pokemon) => (
+          <Card 
+            key={pokemon.id}
+            imagem={pokemon.imagem} 
+            nome={pokemon.name}
+            id={pokemon.id}
+            tipo={pokemon.tipo}
+          />
+        ))}
       </div>
     </>
   )
